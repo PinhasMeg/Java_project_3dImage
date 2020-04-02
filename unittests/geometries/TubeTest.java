75db10de3b5d8546ae4cc62daa3a04555cd26c3f
Yoel <yoelzeit@gmail.com>
2020-04-02 13:32:01 +0300

    1/04/2020
    final recitation 2 + JDK

--- unittests/geometries/TubeTest.java
+++ unittests/geometries/TubeTest.java
@@ -26,16 +26,16 @@ class TubeTest {
         assertEquals(normal, ExpResult);
 
 
-        Point3D point1 = new Point3D(0, 0, 0);
-        Vector vec1 = new Vector(1.0, 0.0, 0.0);
-        Ray ray1 = new Ray(point1, vec1);
-        double radius1 = 1;
-        Tube tube1 = new Tube(radius1, ray1);
-        Point3D p1 = new Point3D(0.0, 1.0, 0.0);
-        Vector normal1 = tube1.getNormal(p1);
-
-
-        Vector ExpResult1 = new Vector(1.0, 0.0, 0.0);
-        assertEquals(normal1, ExpResult1);
+//        Point3D point1 = new Point3D(0, 0, 0);
+//        Vector vec1 = new Vector(1.0, 0.0, 0.0);
+//        Ray ray1 = new Ray(point1, vec1);
+//        double radius1 = 1;
+//        Tube tube1 = new Tube(radius1, ray1);
+//        Point3D p1 = new Point3D(0.0, 1.0, 0.0);
+//        Vector normal1 = tube1.getNormal(p1);
+//
+//
+//        Vector ExpResult1 = new Vector(1.0, 0.0, 0.0);
+//        assertEquals(normal1, ExpResult1);
     }
 }
